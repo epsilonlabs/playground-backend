7905f7cb144a9744698b0aa5603171b575d4b7fc
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2024-02-25 18:57:59 +0000

    Add EML/ECL highlighting and tool/panel defs

--- http-server/src/main/js/highlighting.js
+++ http-server/src/main/js/highlighting.js
@@ -1,11 +1,12 @@
-
-import './eol.js';
-import './etl.js';
-import './flock.js';
-import './evl.js';
-import './epl.js';
+import './ecl.js';
 import './egl.js';
 import './egx.js';
-import './flexmi.js';
 import './emfatic.js';
+import './eml.js';
+import './eol.js';
+import './epl.js';
+import './etl.js';
+import './evl.js';
+import './flexmi.js';
+import './flock.js';
 import './json.js';
