e3a71fc91fd2c0b956e9067ff0c370ace3982db2
Antonio Garcia-Dominguez <a.garcia-dominguez@york.ac.uk>
2024-12-03 12:05:58 +0000

    clone-frontend: move clone to build/ as it should not be version controlled

--- standalone-server/clone-frontend.sh
+++ standalone-server/clone-frontend.sh
@@ -4,11 +4,11 @@
 set -e
 
 GIT_REPO=https://github.com/eclipse-epsilon/epsilon-website.git
-LOCAL_CLONE=epsilon-website
+LOCAL_CLONE=build/epsilon-website
 SOURCE_DIR="$LOCAL_CLONE/mkdocs/docs/playground/"
 TARGET_DIR=build/frontend/public
 
-if ! test -d epsilon-website; then
+if ! test -d "$LOCAL_CLONE"; then
   git clone --depth 1 "$GIT_REPO" "$LOCAL_CLONE"
 else
   pushd "$LOCAL_CLONE" >/dev/null
