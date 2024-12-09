f8be3ad8d6d40e37a17e271668a8ba9ebace9476
Antonio Garcia-Dominguez <a.garcia-dominguez@york.ac.uk>
2024-12-09 10:34:39 +0000

    Use caching for /emfatic2plantuml

--- core/src/main/java/org/eclipse/epsilon/labs/playground/fn/ModelDiagramRenderer.java
+++ core/src/main/java/org/eclipse/epsilon/labs/playground/fn/ModelDiagramRenderer.java
@@ -1,8 +1,10 @@
 package org.eclipse.epsilon.labs.playground.fn;
 
 import java.io.ByteArrayOutputStream;
-import java.nio.charset.Charset;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+import io.micronaut.cache.annotation.Cacheable;
 import org.eclipse.epsilon.egl.EglModule;
 import org.eclipse.epsilon.eol.execute.context.Variable;
 import org.eclipse.epsilon.eol.models.Model;
@@ -13,12 +15,22 @@ import jakarta.inject.Singleton;
 import net.sourceforge.plantuml.FileFormat;
 import net.sourceforge.plantuml.FileFormatOption;
 import net.sourceforge.plantuml.SourceStringReader;
+import org.eclipse.epsilon.labs.playground.fn.emfatic2plantuml.MetamodelDiagramResponse;
 
 @Singleton
 public class ModelDiagramRenderer {
 
     @Inject
+    ModelLoader modelLoader;
+
+    @Inject
     ScriptTimeoutTerminator timeoutTerminator;
+
+    public MetamodelDiagramResponse generateMetamodelDiagram(String emfatic) throws Exception {
+        MetamodelDiagramResponse response = new MetamodelDiagramResponse();
+        response.setMetamodelDiagram(renderPlantUML(emfatic2plantuml(emfatic)));
+        return response;
+    }
 
     public ModelDiagramResponse generateModelDiagram(Model model, Variable... variables) throws Exception {
         EglModule module = new EglModule();
@@ -31,12 +43,7 @@ public class ModelDiagramRenderer {
         try {
             String plantUml = module.execute() + "";
 
-            SourceStringReader reader = new SourceStringReader(plantUml);
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            reader.outputImage(os, new FileFormatOption(FileFormat.SVG));
-            os.close();
-
-            String output = new String(os.toByteArray(), Charset.forName("UTF-8"));
+            String output = renderPlantUML(plantUml);
             ModelDiagramResponse diag = new ModelDiagramResponse();
             diag.setModelDiagram(output);
 
@@ -45,6 +52,34 @@ public class ModelDiagramRenderer {
             module.getContext().getModelRepository().dispose();
             module.getContext().dispose();
         }
+    }
+
+    @Cacheable("emfatic-to-plantuml")
+    protected String emfatic2plantuml(String emfatic) throws Exception {
+        Model model = modelLoader.getInMemoryEmfaticModel(emfatic);
+        model.setName("M");
+
+        EglModule module = new EglModule();
+        module.parse(getClass().getResource("/emfatic2plantuml.egl").toURI());
+        module.getContext().getModelRepository().addModel(model);
+        timeoutTerminator.scheduleScriptTimeout(module);
+
+        try {
+            return module.execute() + "";
+        } finally {
+            module.getContext().getModelRepository().dispose();
+            module.getContext().dispose();
+        }
+    }
+
+    @Cacheable("plantuml-to-svg")
+    protected String renderPlantUML(String plantUml) throws IOException {
+        SourceStringReader reader = new SourceStringReader(plantUml);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        reader.outputImage(os, new FileFormatOption(FileFormat.SVG));
+        os.close();
+
+        return os.toString(StandardCharsets.UTF_8);
     }
 
 }
--- core/src/main/java/org/eclipse/epsilon/labs/playground/fn/emfatic2plantuml/Emfatic2PlantUMLController.java
+++ core/src/main/java/org/eclipse/epsilon/labs/playground/fn/emfatic2plantuml/Emfatic2PlantUMLController.java
@@ -1,66 +1,31 @@
 package org.eclipse.epsilon.labs.playground.fn.emfatic2plantuml;
 
-import java.io.ByteArrayOutputStream;
-import java.nio.charset.Charset;
-
-import org.eclipse.epsilon.egl.EglModule;
-import org.eclipse.epsilon.labs.playground.execution.ScriptTimeoutTerminator;
-import org.eclipse.epsilon.labs.playground.fn.ModelLoader;
-
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import jakarta.inject.Inject;
-import net.sourceforge.plantuml.FileFormat;
-import net.sourceforge.plantuml.FileFormatOption;
-import net.sourceforge.plantuml.SourceStringReader;
+import org.eclipse.epsilon.labs.playground.fn.ModelDiagramRenderer;
 
 @Controller(Emfatic2PlantUMLController.PATH)
 public class Emfatic2PlantUMLController {
     public static final String PATH = "/emfatic2plantuml";
 
     @Inject
-    ScriptTimeoutTerminator timeoutTerminator;
-
-    @Inject
-    ModelLoader loader;
+    ModelDiagramRenderer renderer;
 
     @ExecuteOn(TaskExecutors.IO)
-    @Post("/")
+    @Post
     public MetamodelDiagramResponse render(@Body Emfatic2PlantUMLRequest request) {
-        var response = new MetamodelDiagramResponse();
-
         try {
-            String plantuml = run(request.getEmfatic());
-            response.setMetamodelDiagram(plantuml);
+            return renderer.generateMetamodelDiagram(request.getEmfatic());
         } catch (Throwable e) {
+            var response = new MetamodelDiagramResponse();
             response.setError(e.getMessage());
             response.setOutput(e.getMessage());
-        }
-
-        return response;
-    }
-
-    protected String run(String emfatic) throws Exception {
-        EglModule module = new EglModule();
-        module.parse(getClass().getResource("/emfatic2plantuml.egl").toURI());
-        module.getContext().getModelRepository().addModel(loader.getInMemoryEmfaticModel(emfatic));
-        timeoutTerminator.scheduleScriptTimeout(module);
-
-        try {
-            String plantUml = module.execute() + "";
-
-            SourceStringReader reader = new SourceStringReader(plantUml);
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            reader.outputImage(os, new FileFormatOption(FileFormat.SVG));
-            os.close();
-
-            return new String(os.toByteArray(), Charset.forName("UTF-8"));
-        } finally {
-            module.getContext().getModelRepository().dispose();
-            module.getContext().dispose();
+            return response;
         }
     }
+
 }
